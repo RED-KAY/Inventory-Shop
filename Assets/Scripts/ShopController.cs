633d9faa1d79daee5c20ecfd211da1c676b0dc6f
kaushaldirtcube <kaushal.damania123@gmail.com>
2024-12-09 15:28:07 +0530

    Shop View and Controller
    1. Implemented Shop functionality using Controller for logic and View for Ui

---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController
+{
+    ShopModel _Model;
+    ShopView _View;
+
+    public ShopController(ShopModel m, ShopView v)
+    {
+        _Model = m;
+        _View = v;
+
+        _Model.SetController(this);
+        _View.SetController(this);
+
+        _Model.LoadAllItems();
+
+        PopulateShop();
+    }
+
+    public Dictionary<string, ItemEntry> GetAllItemsEntry()
+    {
+        return _Model.AllItems;
+    }
+
+    public void OnItemSelected(string id) { 
+    
+    }
+
+    public void Buy(string id, int amount)
+    {
+
+    }
+
+    public void PopulateShop()
+    {
+        _View.Refresh();
+    }
+}
+
+
+[Serializable]
+public class ShopModel
+{
+    [SerializeField] private Dictionary<string, ItemEntry> _AllItems;
+    public Dictionary<string, ItemEntry> AllItems => _AllItems;
+    private ShopController _Controller;
+
+    public ShopModel()
+    {
+        _AllItems = new Dictionary<string, ItemEntry>();
+    }
+
+    public void SetController(ShopController controller)
+    {
+        _Controller = controller;
+    }
+
+    public void LoadAllItems()
+    {
+        ItemEntry[] allItems = Resources.LoadAll<ItemEntry>("Items");
+
+        foreach (ItemEntry item in allItems) { 
+            _AllItems.Add(item._Id, item);
+        }
+
+        foreach (KeyValuePair<string, ItemEntry> keyValuePair in _AllItems) {
+            Debug.Log(keyValuePair.Key);
+        }
+    }
+}
---
+++ Assets/Scripts/ShopView.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopView : MonoBehaviour
+{
+    [SerializeField] private GameObject _ItemUIPrefab;
+    [SerializeField] private Transform _ShopContents;
+
+    private ShopController _Controller;
+
+    public void SetController(ShopController controller)
+    {
+        _Controller = controller;
+    }
+
+    public void Populate()
+    {
+        var allItems = _Controller.GetAllItemsEntry();
+        foreach (var item in allItems)
+        {
+            GameObject newItemGO = Instantiate(_ItemUIPrefab, _ShopContents);
+            ItemView newItem = newItemGO.GetComponent<ItemView>();
+            newItem._Button.onClick.AddListener(() => OnItemSelected());
+            newItem._Icon.sprite = item.Value._Icon;
+            newItem._Quantity.text = string.Empty;
+            newItem._Rarity.sprite = GameController.Instance._Rarities[((int)item.Value._Rarity)];
+        }
+
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < _ShopContents.childCount; i++)
+        {
+            Destroy(_ShopContents.GetChild(i).gameObject);
+        }
+    }
+
+    public void Refresh()
+    {
+        Clear();
+        Populate();
+    }
+
+    public void OnItemSelected()
+    {
+
+    }
+}
