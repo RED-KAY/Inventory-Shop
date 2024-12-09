a5ec79576b6a484b3812c2e3a134ffebcde5c66f
kaushaldirtcube <kaushal.damania123@gmail.com>
2024-12-09 15:25:46 +0530

    ItemEntry ScriptableObjects
    1. Implemented ItemEntry class using ScriptableObject namespace.
    2. Created item assets.
    3. Created generic item ui prefab for shop & inventory.

---
+++ Assets/Scripts/ItemEntry.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Materials,
+    Weapons,
+    Consumables,
+    Treasure
+}
+
+public enum Rarity
+{
+    VeryCommon,
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item")]
+public class ItemEntry : ScriptableObject
+{
+    public string _Id;
+    public string _Name;
+    public string _Description;
+    public Sprite _Icon;
+    public ItemType _ItemType;
+    public Rarity _Rarity;
+    public float _Weight;
+    public int _Price;
+    public int _SellingPrice;
+}
